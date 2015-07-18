52f1d019c5c75f274abd013a8e319a6ad7c28611
Frank Hu <humour114@gmail.com>
2015-07-18 13:50:39 +0800

    v1.0 works in windows. using iteration identation

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # GitBook-auto-summary
+
 Automatically update Summary.md of a GitBook repo
+
+v1.0 
+
+tested in Windows
+
+# usage
+
+1. `$ python gitbook-auto-summary.py`
+2. input directory(it should be the *root* directory of GitBook repo), enter
+3. the auto summary file `SUMMARY.md` will be under the same directory.
+
+# examples
+
+```
+# Summary
+
+- child1
+  - [environment](child1\environment.md)
+  - [git-basics](child1\git-basics.md)
+- child2
+  - child233
+    - [mailing-list](child2\child233\mailing-list.md)
+    - [markdown-basics](child2\child233\markdown-basics.md)
+    - [opening](child2\child233\opening.md)
+  - child233 - copy
+    - [mailing-list](child2\child233 - copy\mailing-list.md)
+    - [markdown-basics](child2\child233 - copy\markdown-basics.md)
+    - [opening](child2\child233 - copy\opening.md)
+  - [mailing-list](child2\mailing-list.md)
+  - [markdown-basics](child2\markdown-basics.md)
+  - [opening](child2\opening.md)
+- [gitbook](.\gitbook.md)
+- [introduction](.\introduction.md)
+
+```
--- gitbook-auto-summary.py
+++ gitbook-auto-summary.py
@@ -11,22 +11,31 @@ combine = [] #combined protein list
 dir_input = input('Please input folder path(e.g. D:\Study\Inbox): ')
 print('input directory is: ', dir_input) 
 
-#number_of_csv = 0 # to get how many files are read by us
 # output to flie
-output = open(dir_input + '\\SUMMARY.md', 'w') # output to dir_input with "SUMMARY.md"
+output = open(os.path.join(dir_input, 'SUMMARY.md'), 'w') # output to dir_input with "SUMMARY.md"
 output.write('# Summary\n\n')
 # todo: check summary.md in later versions (if overwrite)
+# todo: seems that only windows uses \, os uses / for directory, need to test
+    # uses os.path.join .normcase
 
-for filename in os.listdir(dir_input): # add list
-    # print(filename)
-    if re.search('.md$', filename): # re to find target markdown files, $ for matching end of filename
-        #number_of_csv += 1
-        print(filename, filename[:-3], 'match') # string cut using pathonic slicing :) (https://docs.python.org/3.4/tutorial/introduction.html#strings)
 
-        if filename != 'SUMMARY.md': # escape SUMMARY.md
-            output.write('- [%s](%s)\n'%(filename[:-3], filename))
-        #csv = open(dir_input + '\\' + filename)
+def output_markdown(dire, iter_length=0):
+    for filename in os.listdir(dire): # add list
+        print('Processing: reading', filename) # output log
+        # print(os.path.join(dire, filename))
+        if os.path.isdir(os.path.join(dire, filename)):
+            # is dir, iteration
+            output.write('  ' * iter_length + '- ' + filename + '\n') # write dir information
+            output_markdown(os.path.join(dire, filename), iter_length + 1) # iteration
+        else:
+            # isfile
+            if re.search('.md$', filename): # re to find target markdown files, $ for matching end of filename
+                # print(filename, filename[:-3], 'match') # string cut using pathonic slicing :) (https://docs.python.org/3.4/tutorial/introduction.html#strings)
+                if filename != 'SUMMARY.md': # escape SUMMARY.md
+                    # print(os.path.join(os.path.relpath(dire, dir_input), filename))
+                    output.write('  ' * iter_length + '- [%s](%s)\n'%(filename[:-3], os.path.join(os.path.relpath(dire, dir_input), filename)))
+                    # iterlength for indent, then output markdown list, uses relpath and join.
 
+output_markdown(dir_input)
 
 print('auto summary finished:)')
-
