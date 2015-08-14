d3287490364d4c65e41e846d3d7e8327159a94d1
Frank Hu <humour114@gmail.com>
2015-08-14 15:02:26 +0800

    version 1.1, readme updated

--- README.md
+++ README.md
@@ -6,35 +6,173 @@ Automatically update SUMMARY.md of a GitBook repo
 
 By [Frank-the-Obscure @ GitHub](https://github.com/Frank-the-Obscure)
 
-# usage
-
-1. $ python gitbook-auto-summary.py *your-GitBook-repo-root*
-  - use argument `-o` to overwrite SUMMARY.md without checking.
-2. the auto summary file `SUMMARY.md`(overwrite) or `SUMMARY-GitBook-auto-summary.md` will be created under the same directory.
-
 Tested with Python 3.4.3 in Windows 7 and OS 10.10
 
-# examples
+
+# Usage
+
+1. `$ python gitbook-auto-summary.py GitBook_repo_root`
+2. The auto summary file `SUMMARY-GitBook-auto-summary.md` will be created under the same directory.
+  - Markdown links of `[filename](file link)` will be created by default.
+  - Files is sorted from files a-z, then folders a-z
+3. Advanced: using parameters
+  - Use `-a` or `--append` to append new markdown files. **Link name in former SUMMARY.md will not be changed**. This is very useful when appending new file to an existing summary.
+  - Use `-o` or `--overwrite` to overwrite on SUMMARY.md. You MUST take care before overwriting!
+
+---
+
+1. 运行`$ python gitbook-auto-summary.py GitBook_repo_root`
+2. 脚本将自动在同一个目录生成 `SUMMARY-GitBook-auto-summary.md` 
+  - 默认 link 为 `[filename](file link)` 
+  - 排序方式为按文件名 a-z, 随后是文件夹名 a-z, 只有含有 markdown 文件的文件夹会被输出
+3. 高级用法: 使用参数
+  - `-a` or `--append` 添加新文件. **已有的文件链接名称不会被修改**. 这一参数在你修改过某些链接名称后, 只想添加新文件时十分有效.
+    - 链接地址会被正确更新
+    - 链接排序也会被更新: 如果对排序有特殊要求, 可考虑运行后 copy 想要的链接到目前的 SUMMARY.md 中.
+  - `-o` or `--overwrite` 直接修改 SUMMARY.md. 由于 SUMMARY.md 会被修改, 请务必小心使用这个功能!
+
+# Examples
+
+### Basic:
+
+eg1. folder tree:
 
 ```
-folder tree:
-.
+eg1.
 ├── README.md
 ├── SUMMARY.md
-├── md
-│   └── SUMMARY.md
-├── nomd
-└── os-and-os-path.md
+├── concept-and-definition.md
+├── language-secret.md
+├── preface.md
+├── rationality.md
+├── study-under-uncertainty.md
+├── twisted.png
+└── wheel-of-python.png
 ```
 
-output SUMMARY.md:
+`python gitbook-auto-summary.py eg1`
+
+
+output SUMMARY-GitBook-auto-summary.md:
 
 ```
 # Summary
 
-- [os-and-os-path](./os-and-os-path.md)
+- [concept-and-definition](./concept-and-definition.md)
+- [language-secret](./language-secret.md)
+- [preface](./preface.md)
+- [rationality](./rationality.md)
 - [README](./README.md)
-- md
-  - [SUMMARY](md/SUMMARY.md)
+- [study-under-uncertainty](./study-under-uncertainty.md)
 
 ```
+
+Note:
+
+- only .md file converted to link
+- SUMMARY.md in input directory is ignored
+
+eg2. folder tree:
+
+```
+eg2.
+├── README.md
+├── SUMMARY-GitBook-auto-summary.md
+├── SUMMARY.md
+├── for-myself-at-16
+│   ├── README.md
+│   ├── SUMMARY.md
+│   ├── twisted.png
+│   └── wheel-of-python.png
+├── nomd
+│   └── Untitled.ipynb
+└── python-1-to-n
+    ├── README.md
+    ├── SUMMARY.md
+    ├── chaos-to-telegram
+    │   └── telegram.md
+    ├── gitbook-auto-summary
+    │   ├── argparse.md
+    │   ├── gitbook-auto-summary.md
+    │   └── os-and-os-path.md
+    └── mahjong
+        ├── chaos-to-bottle.md
+        ├── chaos-to-sae.md
+        └── mahjong.md
+```
+
+`python gitbook-auto-summary.py eg2`
+
+output SUMMARY-GitBook-auto-summary.md:
+
+```
+# Summary
+
+- [README](./README.md)
+- for-myself-at-16
+  - [README](for-myself-at-16/README.md)
+  - [SUMMARY](for-myself-at-16/SUMMARY.md)
+- python-1-to-n
+  - chaos-to-telegram
+    - [telegram](python-1-to-n/chaos-to-telegram/telegram.md)
+  - gitbook-auto-summary
+    - [argparse](python-1-to-n/gitbook-auto-summary/argparse.md)
+    - [gitbook-auto-summary](python-1-to-n/gitbook-auto-summary/gitbook-auto-summary.md)
+    - [os-and-os-path](python-1-to-n/gitbook-auto-summary/os-and-os-path.md)
+  - mahjong
+    - [chaos-to-bottle](python-1-to-n/mahjong/chaos-to-bottle.md)
+    - [chaos-to-sae](python-1-to-n/mahjong/chaos-to-sae.md)
+    - [mahjong](python-1-to-n/mahjong/mahjong.md)
+  - [README](python-1-to-n/README.md)
+  - [SUMMARY](python-1-to-n/SUMMARY.md)
+
+```
+
+Note: 
+
+- SUMMARY.md in subdirectories is not ignored.
+- Directories with no .md file are ignored.
+
+### -a, --append
+
+eg3. folder tree:
+
+```
+eg3.
+├── README.md
+├── SUMMARY.md
+├── concept-and-definition.md
+├── language-secret.md
+
+former SUMMARY.md in eg3:
+
+# Summary
+
+- [语言的诡计](./language-secret.md)
+- [README](./README.md)
+
+```
+
+`python gitbook-auto-summary.py -a eg3`
+
+
+output SUMMARY-GitBook-auto-summary.md:
+
+```
+# Summary
+
+- [concept-and-definition](./concept-and-definition.md)
+- [语言的诡计](./language-secret.md)
+- [README](./README.md)
+
+```
+
+Note:
+
+- Former link name of language-secret.md (语言的诡计) is preserved.
+
+### -o, --overwrite
+
+Output file is the same, while script overwrite SUMMARY.md.
+
+DO take care before overwrite!
--- gitbook-auto-summary.py
+++ gitbook-auto-summary.py
@@ -3,38 +3,42 @@
 # GitBook auto summary
 # summary all .md files in a GitBook folder
 
+import argparse
 import os
 import re
-from sys import argv
-import argparse
 
 def output_markdown(dire, base_dir, output_file, append, iter_depth=0):
     """Main iterator for get information from every file/folder
 
-    i: directory, base dire(for calulate relative path), output file, iter depth.
+    i: directory, base directory(to calulate relative path), 
+       output file name, iter depth.
+    p: Judge is directory or is file, then process .md/.markdown files.
     o: write .md information (with identation) to output_file.
     """
-    for filename in sort_dir_file(os.listdir(dire), base_dir): # add list and sort
-        print('Processing: reading', filename) # output log
-        # print(os.path.join(dire, filename))
+    for filename in sort_dir_file(os.listdir(dire), base_dir): 
+        # add list and sort
+        print('Processing ', filename) # output log
         file_or_path = os.path.join(dire, filename)
-        if os.path.isdir(file_or_path):
-            # is dir, iteration
-            if is_mdfile(file_or_path):
-                # if there is .md files in the folder, output folder name and iteration
-                output_file.write('  ' * iter_depth + '- ' + filename + '\n') # write dir information
-                output_markdown(file_or_path, base_dir, output_file, append, iter_depth + 1) # iteration
-        else:
-            # isfile
-            if re.search('.md$|.markdown$', filename): # re to find target markdown files, $ for matching end of filename
-                # print(filename, filename[:-3], 'match') # string cut using pathonic slicing :) (https://docs.python.org/3.4/tutorial/introduction.html#strings)
-                if filename != 'SUMMARY.md' or iter_depth != 0: # escape SUMMARY.md
-                    # print(os.path.join(os.path.relpath(dire, dir_input), filename))
+        if os.path.isdir(file_or_path): #is dir
+            if mdfile_in_dir(file_or_path):
+                # if there is .md files in the folder, output folder name
+                output_file.write('  ' * iter_depth + '- ' + filename + '\n')
+                output_markdown(file_or_path, base_dir, output_file, append, 
+                                iter_depth + 1) # iteration
+        else: # is file
+            if is_markdown_file(filename): 
+            # re to find target markdown files, $ for matching end of filename
+                if (filename not in ['SUMMARY.md', 
+                                     'SUMMARY-GitBook-auto-summary.md'] 
+                    or iter_depth != 0): # escape SUMMARY.md at base directory
                     output_file.write('  ' * iter_depth + 
-                        '- [%s](%s)\n'%(write_md_filename(filename, append, output_file), os.path.join(os.path.relpath(dire, base_dir), filename)))
-                    # iter length for indent, then output markdown list, uses relpath and join.
+                        '- [{}]({})\n'.format(write_md_filename(filename, 
+                                                                append), 
+                            os.path.join(os.path.relpath(dire, base_dir), 
+                                         filename)))
+                    # iter depth for indent, relpath and join to write link.
 
-def is_mdfile(dire):
+def mdfile_in_dir(dire):
     """Judge if there is .md file in the directory
 
     i: input directory
@@ -45,6 +49,20 @@ def is_mdfile(dire):
             if re.search('.md$|.markdown$', filename):
                 return True
     return False
+
+def is_markdown_file(filename):
+    """ Judge if the filename is a markdown filename
+
+    i: filename
+    o: filename without '.md' or '.markdown'
+    """
+    match = re.search('.md$|.markdown$', filename)
+    if not match:
+        return False
+    elif len(match.group()) is len('.md'):
+        return filename[:-3]
+    elif len(match.group()) is len('.markdown'):
+        return filename[:-9]
 
 def sort_dir_file(listdir, dire):
     # sort dirs and files, first files a-z, then dirs a-z
@@ -59,55 +77,62 @@ def sort_dir_file(listdir, dire):
         list_of_file.append(dire)
     return list_of_file  
 
-def write_md_filename(filename, append, output_file):
-    print(append, filename) #
+def write_md_filename(filename, append):
+    """ write markdown filename
+
+    i: filename and append
+    p: if append: find former list name and return
+       else: write filename
+    """
     if append:
         for line in former_summary_list:
             if re.search(filename, line):
-                print('append', filename)#
                 s = re.search('\[.*\]\(',line)
-                print(s.group()[1:-2])
                 return s.group()[1:-2]
         else:
-            return filename[:-3]        
+            return is_markdown_file(filename)
     else:
-        return filename[:-3]
+        return is_markdown_file(filename)
 
 def main():
-    combine = []
-
     parser = argparse.ArgumentParser()
-    parser.add_argument('-o', '--overwrite', help='overwrite on SUMMARY.md', 
+    parser.add_argument('-o', '--overwrite', 
+                        help='overwrite on SUMMARY.md', 
                         action="store_true")
-    parser.add_argument('-a', '--append', help='append on SUMMARY.md', 
+    parser.add_argument('-a', '--append', 
+                        help='append on SUMMARY.md', 
                         action="store_true")
-    parser.add_argument('directory', help='the directory of your GitBook root')
+    parser.add_argument('directory', 
+                        help='the directory of your GitBook root')
     args = parser.parse_args()
     overwrite = args.overwrite
     append = args.append
     dir_input = args.directory
-    if args.overwrite:
-        print(dir_input, 'overwrite')
-    else:
-        print(dir_input)
-    if args.append:
-        print('append')
+
+    # print information
+    print('GitBook auto summary:', dir_input, end = ' ')
+    if overwrite:
+        print('--overwrite', end = ' ')
+    if append and os.path.exists(os.path.join(dir_input, 'SUMMARY.md')): 
+        #append: read former SUMMARY.md
+        print('--append', end = ' ')
         global former_summary_list
         with open(os.path.join(dir_input, 'SUMMARY.md')) as f:
             former_summary_list = f.readlines()
-            print(former_summary_list)
             f.close()
+    print()
     # output to flie
-    if overwrite == False and os.path.exists(os.path.join(dir_input, 'SUMMARY.md')):
-        # check if there is an SUMMARY.md in directory.
-        output = open(os.path.join(dir_input, 'SUMMARY-GitBook-auto-summary.md'), 'w') # output to dir_input with "SUMMARY.md"
-    else: # no file there
-        output = open(os.path.join(dir_input, 'SUMMARY.md'), 'w') # output to dir_input with "SUMMARY.md"
+    if (overwrite == False and 
+        os.path.exists(os.path.join(dir_input, 'SUMMARY.md'))):
+        # overwrite logic
+        filename = 'SUMMARY-GitBook-auto-summary.md'
+    else:
+        filename = 'SUMMARY.md'
+    output = open(os.path.join(dir_input, filename), 'w')
     output.write('# Summary\n\n')
-    # todo: seems that only windows uses \, os/linux uses / for directory, need to test
     output_markdown(dir_input, dir_input, output, append)
 
-    print('auto summary finished:) ')
+    print('GitBook auto summary finished:) ')
     return 0
 
 if __name__ == '__main__':
